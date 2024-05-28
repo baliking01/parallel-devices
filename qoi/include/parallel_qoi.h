09b82bccee5d2ecbfb4c9b82220b95d8821c90a7
n.balazs <balazsnagy220@gmail.com>
2024-05-28 02:30:21 +0200

    simple rgba encoding test

--- qoi/include/parallel_qoi.h
+++ qoi/include/parallel_qoi.h
@@ -34,14 +34,16 @@ int parallel_qoi_write(const char operation, const char *filename, const void *d
         desc->width * desc->height * (desc->channels + 1) +
         QOI_HEADER_SIZE + sizeof(qoi_padding);
 
-    unsigned char *bytes = (unsigned char *) QOI_MALLOC(max_size);
+    // calloc instead of QOI_MALLOC for cleanly separated data after compression
+    unsigned char *bytes = (unsigned char *) calloc(max_size, sizeof(unsigned char));
 
     const unsigned char *pixels = (const unsigned char *)data;
     int px_len = desc->width * desc->height * desc->channels;
 	int px_end = px_len - desc->channels;
 	int channels = desc->channels;
 
-	printf("px_len: %d\n", px_len);
+	unsigned int chunk_lengths[desc->height];
+    printf("px_len: %d\n", px_len);
 	printf("width: %d\n", desc->width);
 	printf("height: %d\n", desc->height);
 	printf("channels: %d\n", channels);
@@ -49,11 +51,14 @@ int parallel_qoi_write(const char operation, const char *filename, const void *d
     
 	cl_mem pixel_buffer = clCreateBuffer(ocl.context, CL_MEM_READ_ONLY, px_len * sizeof(unsigned char), NULL, NULL);
     cl_mem bytes_buffer = clCreateBuffer(ocl.context, CL_MEM_READ_WRITE, max_size * sizeof(unsigned char), NULL, NULL);
+    cl_mem chunk_lens_buffer = clCreateBuffer(ocl.context, CL_MEM_READ_WRITE, desc->height * sizeof(unsigned int), NULL, NULL);
 
     // TODO: adjust work item sizes in case img_height > CL_DEVICE_MAX_WORK_ITEM_SIZES
     clSetKernelArg(ocl.kernel, 0, sizeof(cl_mem), (void*)&pixel_buffer);
     clSetKernelArg(ocl.kernel, 1, sizeof(cl_mem), (void*)&bytes_buffer);
-    clSetKernelArg(ocl.kernel, 2, sizeof(int), (void*)&desc->width);
+    clSetKernelArg(ocl.kernel, 2, sizeof(cl_mem), (void*)&chunk_lens_buffer);
+    clSetKernelArg(ocl.kernel, 3, sizeof(int), (void*)&desc->width);
+    clSetKernelArg(ocl.kernel, 4, sizeof(int), (void*)&desc->channels);
 
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
@@ -96,6 +101,18 @@ int parallel_qoi_write(const char operation, const char *filename, const void *d
         NULL,
         NULL
     );
+
+    clEnqueueReadBuffer(
+        command_queue,
+        chunk_lens_buffer,
+        CL_TRUE,
+        0,
+        desc->height * sizeof(unsigned int),
+        chunk_lengths,
+        0,
+        NULL,
+        NULL
+    );
     
     // TODO: - add header info
     // 		 - merge compressed segments
@@ -104,9 +121,53 @@ int parallel_qoi_write(const char operation, const char *filename, const void *d
     for (int i = 0; i < 200; i++){
     	printf("%d ", pixels[i]);
     }
+
     printf("\nConverted:\n");
     for (int i = 0; i < 200; i++){
-    	printf("%d ", bytes[i]);
+        printf("%d ", bytes[i]);
+    }
+
+    printf("New\n");
+    int offset;
+    for (int i = 0; i < desc->height; i++){
+        printf("Line: %d\n", i);
+        offset = i * desc->width * (channels + 1);
+        for (int j = 0; j < chunk_lengths[i];){
+            switch (bytes[offset + j]){
+                case QOI_OP_INDEX:
+
+                case QOI_OP_DIFF:
+
+                case QOI_OP_LUMA:
+
+                case QOI_OP_RUN:
+
+                case QOI_OP_RGB:
+                    printf("R:%d G:%d B:%d\n", bytes[offset + j + 1], bytes[offset + j + 2], bytes[offset + j + 3]);
+                    j += 4;
+                    break;
+                case QOI_OP_RGBA:
+                    printf("R:%d G:%d B:%d A:%d\n", bytes[offset + j + 1], bytes[offset + j + 2], bytes[offset + j + 3], bytes[offset + j + 4]);
+                    j += 5;
+                    break;
+                default:
+                    break;
+            }/*
+            if (bytes[offset + j] == QOI_OP_RGB){
+               printf("R:%d G:%d B:%d\n", bytes[offset + j + 1], bytes[offset + j + 2], bytes[offset + j + 3]);
+               j += 4;
+            }
+            else if (bytes[offset + j] == QOI_OP_RGBA){
+                printf("R:%d G:%d B:%d A:%d\n", bytes[offset + j + 1], bytes[offset + j + 2], bytes[offset + j + 3], bytes[offset + j + 4]);
+                 j += 5;
+            }*/
+        }
+
+    }
+
+    printf("\n");
+    for (int i = 0; i < desc->height; i++){
+        printf("%d ", chunk_lengths[i]);
     }
 
     //int r = stbi_write_png("gray.png", desc->width, desc->height, channels, (void*)bytes, 0);
--- qoi/kernels/codec.cl
+++ qoi/kernels/codec.cl
@@ -1,15 +1,69 @@
-__kernel void encode(__global unsigned char *px, __global unsigned char *byte, int width)
+#define QOI_OP_INDEX  0x00 /* 00xxxxxx */
+#define QOI_OP_DIFF   0x40 /* 01xxxxxx */
+#define QOI_OP_LUMA   0x80 /* 10xxxxxx */
+#define QOI_OP_RUN    0xc0 /* 11xxxxxx */
+#define QOI_OP_RGB    0xfe /* 11111110 */
+#define QOI_OP_RGBA   0xff /* 11111111 */
+
+__kernel void encode(__global unsigned char *pixels, __global unsigned char *bytes, __global unsigned int *chunk_lens, int width, int channels)
 {
-	int id = get_global_id(0) * width * 4; // RGBA offset 4
+	/*
+	// grayscale test
+	int id = get_global_id(0) * width * channels; // RGBA offset 4
     
 	for (int i = 0; i < width*4; i+=4){
-		byte[id + i + 0] = 255 - px[id + i + 0];
-        byte[id + i + 1] = 255 - px[id + i + 1];
-        byte[id + i + 2] = 255 - px[id + i + 2];
-        byte[id + i + 3] = 255 - px[id + i + 3];
+		bytes[id + i + 0] = 255 - pixels[id + i + 0];
+        bytes[id + i + 1] = 255 - pixels[id + i + 1];
+        bytes[id + i + 2] = 255 - pixels[id + i + 2];
+        
+        if (channel == 4) bytes[id + i + 3] = 255 - pixels[id + i + 3];
 	}
+	*/
+	
+	// encode pixels
+	int id = get_global_id(0) * width * channels;
+	// byte index, account for tags
+	unsigned int p = get_global_id(0) * width * (channels + 1);
+	unsigned int start = p;
 
+	unsigned char index[64*4] = {0}; // qoi_rgba_t
+	unsigned char px_r, px_g, px_b, px_a, px_prev_r, px_prev_g, px_prev_b, px_prev_a;
 
-    
+	int run = 0;
+	px_r = px_prev_r = 0;
+	px_g = px_prev_g = 0;
+	px_b = px_prev_b = 0;
+	px_a = px_prev_a = 255;
+	
+	
+	for (int px_pos = 0; px_pos < width * channels; px_pos += channels){
+		px_r = pixels[id + px_pos + 0];
+		px_g = pixels[id + px_pos + 1];
+		px_b = pixels[id + px_pos + 2];
+
+		if (channels == 4) {
+			px_a = pixels[id + px_pos + 3];
+		}
+
+		if (px_a == px_prev_a){
+			bytes[p++] = QOI_OP_RGB;
+			bytes[p++] = px_r;
+			bytes[p++] = px_g;
+			bytes[p++] = px_b;
+		}
+		else {
+			bytes[p++] = QOI_OP_RGBA;
+			bytes[p++] = px_r;
+			bytes[p++] = px_g;
+			bytes[p++] = px_b;
+			bytes[p++] = px_a;
+		}
+
+		px_prev_r = px_r;
+		px_prev_g = px_g;
+		px_prev_b = px_b;
+		px_prev_a = px_a;
+	}
+	chunk_lens[get_global_id(0)] = p - start;
 }
 
